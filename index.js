7d43e0b9d1066df863ecda4229007c63e5c7dbab
sonjasun1 <60373743+sonjasun1@users.noreply.github.com>
2020-02-16 17:46:33 -0600

    Assignment3
    Index.js Upload

---
+++ index.js
@@ -0,0 +1,78 @@
+// JavaScript Document
+const item = (
+	itemId: 'def5cd03-7bf4-49e9-8f0b-380f90c86ec1'
+    name: "Drake Bulldogs Sweatshirt"
+    description: "Get comfortable in these sweats and look fly while wearing them" ,
+    price: 19.99 ,
+	size: 'Medium' , 
+);
+
+
+const firstName = "Jason";
+const lastName = "Bradley";
+//const customerId: 52200d8d-87b6-42e6-8906-a9972986dc9f
+
+const customer = {
+	firstName, 
+	lastName,
+	email : '$(firstName.toLowerCase*()}.$(LastName.toLowerCase()}@drake.edu, 
+	phoneNumber: '+15155555555')'
+};
+
+const cart = {
+	cartId: 'da46f758-f84a-460c-a6e4-93e6cc46401f',
+	customerId: '52200d8d-87b6-42e6-8906-a9972986dc9f',
+	createdDate: '2020-01-30',
+	purchasedDate: '2025-02-01'
+}
+
+
+const cartItem = {
+	itemID: 'def5cd03-7bf4-49e9-8f0b-380f90c86ec1' , 
+	cartID: 'da46f758-f84a-460c-a6e4-93e6cc46401f' , 
+	quantity: 1
+};
+
+const item2 = {
+	itemId: '504550bc-d21e-43c3-8767-0c1bd59a173c'
+    name: "Drake Bulldogs Sweatpants"
+    description: "Get comfortable in these sweats and look fly while wearing them" ,
+    price: 19.99 , 
+	size: 'Large' , 
+};
+
+const firstName = "Sonya";
+const lastName = "Benzm";
+//const customerId: a263b300-6101-490b-abab-d35698b4d8cc
+
+
+const customer2 = {
+	firstName, 
+	lastName,
+	email : '$(firstName.toLowerCase*()}.$(LastName.toLowerCase()}@gmail.com, 
+	phoneNumber: '+15073402042')'
+};
+
+const cart2 = {
+	cartId: 'e9c50b0b-37c1-45cc-ac8c-adfafa12fd24',
+	customerId: 'a263b300-6101-490b-abab-d35698b4d8cc',
+	createdDate: '2020-01-30',
+	purchasedDate: '2025-02-01'
+}
+
+const cartItem2 = {
+	itemID: '504550bc-d21e-43c3-8767-0c1bd59a173c' , 
+	cartID: 'e9c50b0b-37c1-45cc-ac8c-adfafa12fd24' , 
+	quantity: 1
+};
+
+
+console.log('item', item);
+console.log('customer', customer);
+console.log('cart', cart);
+console.log('cartItem', cartItem)
+
+console.log('item2', item2);
+console.log('customer2', customer2);
+console.log('cart2', cart2);
+console.log('cartItem2', cartItem2);
